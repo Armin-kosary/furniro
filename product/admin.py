b7092d464e0815b8e7d8e184e778e3d30c3138ee
Armin Kosari <Armynkosari@gmail.com>
2024-10-17 15:15:19 +0330

    create product model and migrate

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Product, ProductCategory, ProductColor, ProductSize, ProductTag
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(ProductTag)
+admin.site.register(ProductSize)
+admin.site.register(ProductColor)
+admin.site.register(ProductCategory)
--- product/models.py
+++ product/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from uuid import uuid4
 # Create your models here.
+
+class ProductTag(models.Model):
+    title = models.CharField(max_length=20)
+
+class ProductCategory(models.Model):
+    title = models.CharField(max_length=20)
+
+class ProductColor(models.Model):
+    title = models.CharField(max_length=20)
+
+class ProductSize(models.Model):
+    title = models.CharField(max_length=20)
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=100)
+    price = models.IntegerField()
+    discount = models.IntegerField(blank=True)
+    short_description = models.TextField()
+    full_description = models.TextField()
+    size = models.ManyToManyField(ProductSize)
+    color = models.ManyToManyField(ProductColor)
+    category = models.ForeignKey(ProductCategory, on_delete=models.SET_NULL, null=True)
+    tags = models.ManyToManyField(ProductTag)
+    score = models.IntegerField()
+    product_code = models.CharField(max_length=50)
+
+    def save(self, *args, **kwargs):
+        if not self.product_code:
+            self.product_code = uuid4()
+        super(Product, self).save(*args, **kwargs)
